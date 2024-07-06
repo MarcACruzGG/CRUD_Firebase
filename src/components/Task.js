ab6fba55503298d200b31b7c7b10110b97b71154
Marco Antonio Cruz Gomez <91018264+MarcACruzGG@users.noreply.github.com>
2024-07-05 20:16:44 -0600

    Update

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,46 +4,44 @@ import { database } from '../../config/fb';
 import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { AntDesign } from '@expo/vector-icons';
 
-export default function Product({
+export default function Task({
     id,
     emoji,
     name,
-    price,
-    isSold,
+    completed,
 }) {
 
     const onDelete = () => {
-        const docRef = doc(database, 'products', id);
+        const docRef = doc(database, 'tasks', id);
         deleteDoc(docRef);
     }
 
     const onEdit = () => {
-        const docRef = doc(database, 'products', id);
+        const docRef = doc(database, 'tasks', id);
         updateDoc(docRef, {
-            isSold: true,
+            completed: true,
         });
     }
 
     return(
         <RN.View>
-            <RN.View style={styles.productContainer}>
+            <RN.View style={styles.taskContainer}>
                 <RN.View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                     <RN.Text style={styles.emoji}>{emoji}</RN.Text>
                     <AntDesign onPress={onDelete} name="delete" size={24} color="black" />
                 </RN.View>
                 <RN.Text style={styles.name}>{name}</RN.Text>
-                <RN.Text style={styles.price}>${price}</RN.Text>
-                {isSold ? (
+                {completed ? (
                     <RN.TouchableOpacity 
                     style={[styles.button, {backgroundColor: 'gray'}]}>
-                    <RN.Text style={styles.buttonText}>Sold</RN.Text>
+                    <RN.Text style={styles.buttonText}>Realizada</RN.Text>
                 </RN.TouchableOpacity>
                 )
                 : (
                     <RN.TouchableOpacity 
                     onPress={onEdit}
                     style={styles.button}>
-                    <RN.Text style={styles.buttonText}>Purchase</RN.Text>
+                    <RN.Text style={styles.buttonText}>Marcar como Realizada</RN.Text>
                 </RN.TouchableOpacity>
                 )}
                 
@@ -53,7 +51,7 @@ export default function Product({
 }
 
 const styles = RN.StyleSheet.create({
-    productContainer: {
+    taskContainer: {
         padding: 16,
         backgroundColor: '#fff',
         margin: 16,
@@ -65,11 +63,6 @@ const styles = RN.StyleSheet.create({
     name: {
         fontSize: 32,
         fontWeight: 'bold',
-    },
-    price: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: 'gray',
     },
     button: {
         backgroundColor: '#0FA5E9',
